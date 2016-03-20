4508918d2b0835609c0dcaf8dc146acbeed1e2a1
Robert Miller <romille@clarkson.edu>
2016-03-20 19:23:06 -0400

    Added functionality to printer class and added a test case in main (UNTESTED BUT COMPILES)
    added code so printer class can print a string with the startPrint
    method. Don't know if this works because I don't have a printer to test.
    It should format the string to fit on one line but it's all in theory.
    Also the startPrint method formats the string to be the paper sized
    specified by the client.

--- src/autoprint/Main.java
+++ src/autoprint/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		Printer p = new Printer();
 		Networker n = new Networker();
+		p.init();
+		p.startPrint("Really long string to print that hopefully will work and stuff but i really dont know as I am unable to get to a printer right now but maybe I can see if someone I know has a printer I can use for great convinience.");
+		
 	}
 
 }
--- src/autoprint/printer.java
+++ src/autoprint/printer.java
@@ -3,12 +3,64 @@ package autoprint;
 import java.awt.*;
 import java.awt.print.*;
 
-public class printer implements Printable {
-	PrinterJob printJob = PrinterJob.getPrinterJob();
+public class Printer implements Printable {
+	PrinterJob printJob;
+	String stringToPrint;
+	public void init(){
+	printJob = PrinterJob.getPrinterJob();
+	}
 	
-	@Override
+	public void startPrint(String s){
+		stringToPrint = "<html>"+s+"</html>";
+		boolean ok = printJob.printDialog();
+		if (ok){
+			PageFormat pf = printJob.defaultPage();
+			Paper p = pf.getPaper();
+			double width = 5.5d * 72d;
+			double height = 8.5d * 72d;
+			double margin = 0.2d * 72d;
+			p.setSize(width, height);
+			p.setImageableArea(
+					margin,
+					margin,
+					width - (margin*2),
+					height - (margin*2));
+			pf.setOrientation(PageFormat.PORTRAIT);
+			pf.setPaper(p);
+			Book pBook = new Book();
+			pBook.append(this, pf);
+			printJob.setPageable(pBook);
+			
+			try{
+				printJob.print();
+			}catch(PrinterException pe){
+				System.err.println("Print failed");
+				System.err.println("Exception:\n" + pe);
+			}
+		}
+	}
+	
+	
+
+	
+
 	public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
-		// TODO Auto-generated method stub
-		return 0;
+        
+		Font font = new Font("Serif", Font.PLAIN, 10);
+        FontMetrics metrics = graphics.getFontMetrics(font);
+        int lineHeight = metrics.getHeight();
+        
+        int linesPerPage = (int) pageFormat.getImageableHeight()/lineHeight;
+        
+        
+		if (pageIndex >= 1) {
+            return Printable.NO_SUCH_PAGE;
+        }
+		Graphics2D g2d = (Graphics2D)graphics;
+		g2d.translate((int) pageFormat.getImageableX(), (int) pageFormat.getImageableY());
+		double width = pageFormat.getImageableWidth();
+		double height = pageFormat.getImageableHeight();
+		graphics.drawString(stringToPrint, 0, 0);
+		return Printable.PAGE_EXISTS;
 	}
 }
