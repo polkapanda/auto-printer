d7bea2f83c23f7747cddbd7fddabf3b5d17b06cc
Robert Miller <romille@clarkson.edu>
2016-03-20 17:43:50 -0400

    printer class added
    added base of printer class

---
+++ src/printer/printer.java
@@ -0,0 +1,14 @@
+package printer;
+
+import java.awt.*;
+import java.awt.print.*;
+
+public class printer implements Printable {
+	PrinterJob printJob = PrinterJob.getPrinterJob();
+	
+	@Override
+	public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
