b4e8dada321be876055b6ace0ae9a07b83373e94
Robert Miller <romille@clarkson.edu>
2016-03-20 20:35:31 -0400

    Changed print method
    changed how printing long things worked. Still untested since no
    printer. hopefully able to span multiple pages now

--- src/autoprint/printer.java
+++ src/autoprint/printer.java
@@ -2,16 +2,19 @@ package autoprint;
 
 import java.awt.*;
 import java.awt.print.*;
+import java.util.Vector;
 
 public class Printer implements Printable {
 	PrinterJob printJob;
 	String stringToPrint;
+	int[] pageBreaks = null;
+	Vector<String> formattedString = null;
 	public void init(){
 	printJob = PrinterJob.getPrinterJob();
 	}
 	
 	public void startPrint(String s){
-		stringToPrint = "<html>"+s+"</html>";
+		stringToPrint = s;
 		boolean ok = printJob.printDialog();
 		if (ok){
 			PageFormat pf = printJob.defaultPage();
@@ -51,16 +54,58 @@ public class Printer implements Printable {
         int lineHeight = metrics.getHeight();
         
         int linesPerPage = (int) pageFormat.getImageableHeight()/lineHeight;
+        double width = pageFormat.getImageableWidth();
         
+        if (pageBreaks == null){
+        	//breaks the text file from one big string into an array of strings
+        	for (String line : stringToPrint.split("\n")){
+        		if (metrics.stringWidth(line) > width){
+        			String shortLine = new String();
+        			for (String word : line.split(" ")){
+        				//adds words until the line length is met
+        				if (metrics.stringWidth(shortLine + word) > width)
+        					shortLine += word;
+        				//adds the shorter line to a vector and then resets the line to the new word
+        				else{
+        					formattedString.add(shortLine);
+        					shortLine = word;
+        				}
+        			}
+        			//adds the last line if the initial line is too long
+        				formattedString.add(shortLine);
+        		}else{
+        			//adds the line if it is shorter than the line width
+        			formattedString.add(line);
+        		}
+        	}
+        	//gets place of page breaks
+        	int numBreaks = (formattedString.size()-1)/linesPerPage;
+        	pageBreaks = new int[numBreaks];
+        	for(int b = 0; b < numBreaks; b++){
+        		pageBreaks[b] = (b+1)*linesPerPage;
+        	}
+
+
+        }
         
 		if (pageIndex >= 1) {
             return Printable.NO_SUCH_PAGE;
         }
+		
 		Graphics2D g2d = (Graphics2D)graphics;
 		g2d.translate((int) pageFormat.getImageableX(), (int) pageFormat.getImageableY());
-		double width = pageFormat.getImageableWidth();
-		double height = pageFormat.getImageableHeight();
-		graphics.drawString(stringToPrint, 0, 0);
+		
+		int y = 0;
+		int start = (pageIndex == 0) ? 0 //if
+									 : pageBreaks[pageIndex - 1]; //else
+		int end = (pageIndex == pageBreaks.length) ? formattedString.size() //if
+												   : pageBreaks[pageIndex]; //else
+		//prints a page of lines
+		for(int line = start; line < end; line++){
+			y += lineHeight;
+			graphics.drawString(formattedString.get(line), 0, y);
+		}
+		
 		return Printable.PAGE_EXISTS;
 	}
 }
